dfa16cd5a981f6bb043c46647830dff4793f0cb2
avielmoshe <mosheavieli@gmail.com>
2024-07-26 00:14:41 +0300

    task 7

---
+++ 25.07/task 7/main.js
@@ -0,0 +1,39 @@
+// 1
+let age = 30;
+let canVote;
+if (age >= 18) {
+  canVote = true;
+} else {
+  canVote = false;
+}
+console.log(canVote);
+// 2
+let temperature = 28;
+let weather;
+if (temperature < 0) {
+  weather = "freezing";
+} else {
+  weather = "not freezing";
+}
+console.log(weather);
+// 3
+let score = 65;
+let result;
+if (score >= 60) {
+  result = "pass";
+} else {
+  result = "fail";
+}
+console.log(result);
+// 4
+let grade = 95;
+let letterGrade;
+if (grade >= 90) {
+  letterGrade = "A";
+} else if (grade >= 80) {
+  letterGrade = "B";
+} else {
+  letterGrade = "C";
+}
+console.log(letterGrade);
+//   5
