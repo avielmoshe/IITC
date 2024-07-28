b8c7467f8aa079f92431b90dc78334e8eda30c0a
avielmoshe <mosheavieli@gmail.com>
2024-07-28 12:57:28 +0300

    1

---
+++ 25.07/atm/main.js
@@ -0,0 +1,118 @@
+let balance = 20000;
+let pinCode = "1234";
+const maxWithdrawal = 10000;
+let conter = 0;
+
+function verify(entrPin, pinCode) {
+  if (entrPin === pinCode) {
+    alert("the pin code is " + true);
+  } else {
+    for (let i = 1; i < 3; i++) {
+      conter += i;
+      if (conter < 4) {
+        entrPin = prompt("the pin code is " + false + " enter pincode");
+        verify(entrPin, pinCode);
+      } else alert("account lock");
+    }
+  }
+}
+
+let entrPin = prompt("enter pincode");
+verify(entrPin, pinCode);
+
+function balanceIs(balance) {
+  alert(`the balance is: ${balance}$`);
+}
+balanceIs(balance);
+
+function withdrwal(maxWithdrawal, balance, amountof) {
+  if (amountof <= 0) {
+    amountof = prompt("invalid amount of withdrwal ,enter amount of withdrwal");
+    withdrwal(maxWithdrawal, balance, amountof);
+  } else if (amountof > maxWithdrawal) {
+    amountof = prompt(
+      "invalid amount of withdrwal you can't withdrawl above the max withdrwal, enter amount of withdrwal"
+    );
+    withdrwal(maxWithdrawal, balance, amountof);
+  } else if (amountof > balance) {
+    amountof = prompt(
+      "invalid amount of withdrwal you can't withdrawl above your balance, enter amount of withdrwal"
+    );
+    withdrwal(maxWithdrawal, balance, amountof);
+  } else
+    alert(
+      `you withdrwal from your account ${amountof}$ your new balance is ${
+        balance - amountof
+      }$ `
+    );
+}
+let amountof = prompt("enter amount of withdrwal");
+withdrwal(maxWithdrawal, balance, amountof);
+if (amountof < balance && amountof < maxWithdrawal && amountof > 0) {
+  balance -= amountof;
+} else balance = balance;
+console.log(balance);
+
+// אני פה
+function deposit(balance, amountofDe) {
+  if (amountofDe <= 0) {
+    amountofDe = Number(
+      prompt("invalid amount of deposit, enter amount of deposit")
+    );
+    deposit(balance, amountofDe);
+  } else
+    alert(
+      `you deposit to your account ${amountofDe}$ your new balance is ${
+        balance + amountofDe
+      }$ `
+    );
+}
+let amountofDe = Number(prompt("enter amount of deposit"));
+deposit(balance, amountofDe);
+if (amountof > 0) {
+  balance += amountofDe;
+} else balance = balance;
+console.log(balance);
+
+// function mainAtm(
+//   entrPin,
+//   pinCode,
+//   balance,
+//   amountof,
+//   amountofDe,
+//   maxWithdrawal
+// ) {
+entrPin = Number(prompt("enter pincode"));
+
+if (verify(entrPin, pinCode) === "the pin code is " + true);
+{
+  function mainAtm(
+    entrPin,
+    pinCode,
+    balance,
+    amountof,
+    amountofDe,
+    maxWithdrawal
+  ) {
+    let optionts = prompt(
+      "enter your option-check balance,withdraw or deposit"
+    );
+    if (optionts === "check balance") {
+      balanceIs(balance);
+    } else if (optionts === "withdraw") {
+      amountof = prompt("enter amount of withdrwal");
+      withdrwal(maxWithdrawal, balance, amountof);
+    } else if (optionts === "deposit") {
+      amountofDe = Number(prompt("enter amount of deposit"));
+      deposit(balance, amountofDe);
+    } else {
+      optionts = prompt(
+        "invalid optionts, enter your option-check balance,withdraw or deposit"
+      );
+      mainAtm(entrPin, pinCode, balance, amountof, amountofDe, maxWithdrawal);
+    }
+  }
+  mainAtm(entrPin, pinCode, balance, amountof, amountofDe, maxWithdrawal);
+}
+entrPin = Number(prompt("enter pincode"));
+mainAtm(entrPin, pinCode, balance, amountof, amountofDe, maxWithdrawal);
---
+++ 28.7/in/main.js
@@ -0,0 +1,22 @@
+// 1
+for (let i = 10; i > 0; i--) {
+  console.log(i);
+}
+// 2
+for (let i = 2; i < 21; i += 2) {
+  console.log(i);
+}
+// 3
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+  sum += i;
+}
+console.log(sum);
+
+// 4
+for (let i = 1; i <= 10; i++) {
+  console.log(i*5);
+}
+text="*"
+for (let i = 1; i <= 5; i++) {
+  console.log((text.repeat(i)));}
