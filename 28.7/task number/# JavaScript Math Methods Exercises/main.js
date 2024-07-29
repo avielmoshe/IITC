0604d03ff54ebf658a67d7cab4346458abde6872
avielmoshe <mosheavieli@gmail.com>
2024-07-29 14:35:29 +0300

    numbers

---
+++ 28.7/task number/# JavaScript Math Methods Exercises/main.js
@@ -0,0 +1,55 @@
+// 1
+let num = 4.7;
+console.log(Math.round(num));
+// 2
+num = 16;
+console.log(Math.sqrt(num));
+// 3
+console.log(Math.pow(2, 3));
+// 4
+console.log(Math.abs(-5));
+// 5
+num = 4.9;
+console.log(Math.floor(num));
+// 6
+num = 4.1;
+
+console.log(Math.ceil(num));
+// 7
+console.log(Math.random() * 1);
+// 8
+console.log(Math.max(5, 10, 15));
+// 9
+console.log(Math.min(5, 10, 15));
+// 10
+console.log(Math.sin(90));
+// 11
+console.log(Math.cos(0));
+// 12
+console.log(Math.tan(45));
+// 13
+console.log(Math.log(2));
+// 14
+console.log(Math.exp(2));
+// 15
+console.log(Math.trunc(3.7));
+// 16
+console.log(Math.floor(Math.random() * 10 + 1));
+// 17
+console.log(Math.asin(0.5));
+// 18
+console.log(Math.hypot(4, 4));
+// 19
+// 20
+console.log(Math.sign(-15));
+// 21
+console.log(Math.pow(27, 1 / 3));
+// 22
+console.log(Math.log10(100));
+// 23
+console.log(5 ** 2 * Math.PI);
+// 24
+console.log(Math.atan2(1, 1));
+// 25
+console.log(Math.round(4.5));
+console.log(Math.round(5.5));
