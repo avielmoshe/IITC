e7e959678bdbaf0649df91800277abef7f4e3be8
avielmoshe <mosheavieli@gmail.com>
2024-08-04 16:32:54 +0300

    1

--- 04.08/in/main.js
+++ 04.08/in/main.js
@@ -39,3 +39,48 @@ function createPersons(arr) {
   return newArr;
 }
 console.log(createPersons(names));
+
+// 4
+/* 
+  Write a function "groupBy" that takes 
+  an array of objects and a key.
+  returns an object where each key is a unique value 
+  from the employees array and the corresponding value 
+  is an array containing the employees that belong to that key.
+  Example:
+*/
+let employees = [
+  { name: "John Doe", department: "Engineering", yearsOfExp: 5 },
+  { name: "Jane Smith", department: "Marketing", yearsOfExp: 8 },
+  { name: "Peter Johnson", department: "Engineering", yearsOfExp: 5 },
+  { name: "Lucy Brown", department: "Marketing", yearsOfExp: 10 },
+  { name: "Mike Davis", department: "Engineering", yearsOfExp: 3 },
+  { name: "Sara Wilson", department: "Marketing", yearsOfExp: 8 },
+];
+
+function groupBy(array, key) {
+  let group = {};
+  for (let i = 0; i < array.length; i++) {
+    if (group[array[i][key]] === undefined) {
+      group[array[i][key]] = [];
+    }
+    group[array[i][key]].push(array[i]);
+  }
+  return group;
+}
+console.log(groupBy(employees, "department"));
+
+/*
+{
+  Engineering: [
+    { name: 'John Doe', department: 'Engineering', yearsOfExp: 5 },
+    { name: 'Peter Johnson', department: 'Engineering', yearsOfExp: 5 },
+    { name: 'Mike Davis', department: 'Engineering', yearsOfExp: 3 }
+  ],
+  Marketing: [
+    { name: 'Jane Smith', department: 'Marketing', yearsOfExp: 8 },
+    { name: 'Lucy Brown', department: 'Marketing', yearsOfExp: 10 },
+    { name: 'Sara Wilson', department: 'Marketing', yearsOfExp: 8 }
+  ]
+}
+*/
