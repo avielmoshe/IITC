ad864a693e46ac07f732b72a62b7f6ba917eee04
avielmoshe <mosheavieli@gmail.com>
2024-09-09 18:03:36 +0300

    1

---
+++ SQL_NODE/tests/app.test.js
@@ -0,0 +1,57 @@
+const request = require("supertest");
+const app = require("../server.js"); // Adjust this path to point to your Express app
+
+describe("GET /data", () => {
+  it("should respond with JSON data", async () => {
+    const response = await request(app).get("/data");
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBeDefined();
+
+    // expect(response.body.token).toBeDefined();
+  });
+
+  it("should respond with status code 200", async () => {
+    const response = await request(app).get("/data");
+    expect(response.statusCode).toBe(200);
+  });
+});
+
+describe("add", () => {
+  test("return success", () => {
+    expect(app.add()).toBe("success");
+  });
+});
+
+describe("sum", () => {
+  test("sum two positive numbers", () => {
+    expect(app.sum(1, 3)).toBe(4);
+  });
+});
+describe("sum", () => {
+  test("sum two positive numbers", () => {
+    expect(app.sum(1.5, 3.5)).toBe(5);
+  });
+});
+
+describe("updateAge", () => {
+  test("update age", () => {
+    expect(app.updateAge(18)).toBe(18);
+  });
+});
+
+describe("addCategory", () => {
+  test(" addCategory", () => {
+    expect(app.addCategory("gadgets")).toBe("gadgets");
+  });
+});
+
+// // describe('POST /data', () => {
+// //     it('should respond with a successful message', async () => {
+// //         const mockData = { key: 'value' };
+// //         const response = await request(app)
+// //             .post('/data')
+// //             .send(mockData);
+// //         expect(response.statusCode).toBe(200);
+// //         expect(response.body.message).toBe('Data successfully sent via Axios, and Selenium task completed');
+//     });
+// });
