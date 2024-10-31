f3e9e11f6db1e55f6a8c3be37406670151a87bef
avielmoshe <mosheavieli@gmail.com>
2024-10-31 14:32:44 +0200

    done

--- node.js/testMyApi/routes/jokesRoute.js
+++ node.js/testMyApi/routes/jokesRoute.js
@@ -1,5 +1,13 @@
 import express from "express";
 import Joke from "../models/jokeModel.js";
+import {
+  deleteJoke,
+  getAllJoke,
+  getRandomJoke,
+  getJokeById,
+  createNewJoke,
+  updateJoke,
+} from "../controllers/jokeController.js";
 
 // import authUser from "../middleware/auth.js";
 
@@ -8,103 +16,16 @@ const router = express.Router();
 // router.use(authUser);
 
 // Jokes Routes
-router.get("/", async (req, res) => {
-  try {
-    const jokes = await Joke.find({});
-    if (!jokes.length) {
-      return res.status(404).send({
-        mes: "add new joke to see the jokes",
-      });
-    }
-    res.status(201).send(jokes);
-  } catch (error) {
-    res.status(500).send("UnKnow server error");
-  }
-});
+router.get("/", getAllJoke);
 
-router.get("/random", async (req, res) => {
-  try {
-    const jokes = await Joke.find({});
-    if (!jokes.length) {
-      res.status(404).send({
-        mes: "add new joke to see the jokes",
-      });
-    }
-    const randomJoke = await Joke.aggregate([
-      {
-        $sample: {
-          size: 1,
-        },
-      },
-    ]);
-    res.status(201).send(randomJoke[0]);
-  } catch (error) {
-    res.status(500).send("UnKnow server error");
-  }
-});
+router.get("/random", getRandomJoke);
 
-router.get("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const joke = await Joke.findById(id);
-    if (!joke) {
-      return res.status(404).send({ error: "Joke not found" });
-    }
-    res.status(201).send(joke);
-  } catch (error) {
-    console.error("Error finding joke by ID:", error);
-    res.status(500).json({ error: "Server error" });
-  }
-});
+router.get("/:id", getJokeById);
 
-router.post("/", async (req, res) => {
-  const { joke } = req.body;
-  if (!joke) {
-    res.status(400).json({ error: "joke is required" });
-  }
-  const newJoke = new Joke({
-    joke: req.body.joke,
-  });
-  await newJoke.save();
-  res.status(201).send({
-    msg: "Good Job!",
-  });
-});
+router.post("/", createNewJoke);
 
-router.patch("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updatedJoke = await Joke.findByIdAndUpdate(id, req.body, {
-      new: true,
-    });
-    if (!updatedJoke) {
-      res.status(404).send({ error: "Joke not found" });
-    }
-    res.status(201).send({
-      message: "Joke updated successfully",
-      joke: updatedJoke,
-    });
-  } catch (error) {
-    console.error("Error updating joke by ID:", error);
-    res.status(500).json({ error: "Server error" });
-  }
-});
+router.patch("/:id", updateJoke);
 
-router.delete("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deletedJoke = await Joke.findByIdAndDelete(id);
-    if (!deletedJoke) {
-      return res.status(404).json({ error: "Joke not found" });
-    }
-    res.json({
-      message: "Joke deleted successfully",
-      joke: deletedJoke,
-    });
-  } catch (error) {
-    console.error("Error deleting joke by ID:", error);
-    res.status(500).json({ error: "Server error" });
-  }
-});
+router.delete("/:id", deleteJoke);
 
 export default router;
